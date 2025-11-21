06d3540b361b0134200dbaccef8c7a44a97ecd98
Amr Reda <amr.reda2014@yahoo.com>
2025-11-21 22:58:49 +0200

    connect project with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sehaty_application/sehaty_app.dart';
 
-void main() {
-  runApp(const SehatyApp());
+import 'firebase_options.dart';
+
+void main() async{
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform);
+      runApp(const SehatyApp());
 }
 
 
